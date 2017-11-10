b0c28f473278bbfd949f3f31e60a3baa4c142516
victoriacusto <victoriacusto@gmail.com>
2017-11-10 10:15:26 -0300

    header y footer

---
+++ Sprint4/footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<hmtl>
+	<head>
+		<link rel="stylesheet" href="master.css">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<style>
+		@import url('https://fonts.googleapis.com/css?family=Roboto:400,900');
+		</style>
+	</head>
+		<footer class="main-footer">
+			<nav class="nav-footer">
+				<ul>
+					<li><a href="?pagina=quines-somos">QUIENES SOMOS</a></li>
+					<li><a href="?pagina=preguntasFrecuentes">FAQ</a></li>
+					<li><a href="?pagina=contacto">CONTACTO</a></li>
+				</ul>
+			</nav>
+		</footer>
+</div>
+	</body>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</hmtl>
--- footer.php
+++
@@ -1,6 +0,0 @@
-	<footer class="text-muted">
-		Digital House 2017
-	</footer>
-</div>
-</body>
-</html>
